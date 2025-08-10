a0a45ebd71e59f867cc209d967b52665f8dc8f1e
Harsh Devisha <harshdevisha9111@gmail.com>
2025-08-09 20:08:07 -0700

    must: math

---
+++ CSES/math/Exponentiation.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <set>
+#include <utility>
+#include <unordered_set>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <queue>
+
+#define ll long long
+
+#define vec vector
+#define pll pair<ll, ll>
+#define plll pair<ll, pll>
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define vi vector<int>
+#define vii vector<pii>
+#define vl vector<ll>
+#define vvl vector<vl>
+#define vll vector<pll>
+#define pb push_back
+#define FOR(i, a, b) for(ll i=(a); i<=(b); ++i)
+#define FORD(i, a, b) for(ll i=(a); i>=(b); --i)
+#define SORT(a) sort((a).begin(), (a).end())
+#define println(a) (cout << (a) << endl)
+#define print(a) (cout << (a))
+#define REV(a) reverse((a).begin(), (a).end())
+
+const ll MOD = 1e9+7;
+
+const int inf = 1e9;
+const ll ll_inf = 1e18;
+const int dirs[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+const char cdirs[4] = {'R', 'L', 'D', 'U'};
+
+using namespace std;
+
+int main() {
+    ll n; cin >> n;
+    while (n--) {
+        ll a, b; cin >> a >> b;
+        if (b == 0) { println(1); continue; }
+        if (b == 1) { println(a); continue; }
+        ll res=1;
+        while (b) {
+            if (b & 1) res = (res*a)%MOD;
+            a = (a*a)%MOD;
+            b >>= 1;
+        }
+        println(res);
+    }
+    return 0;
+}
+
