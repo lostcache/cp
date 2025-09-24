36c7588d280af468ef67c5ed7cfcec152ad6e9af
Harsh Devisha <harshdevisha9111@gmail.com>
2025-09-23 17:15:33 -0700

    cf

---
+++ codeforces/900-1000/2140B.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int inf = 1e9 + 7;
+const long long ll_inf = 1e19 + 7;
+
+void solve() {
+    long long n;
+    cin >> n;
+    cout << 2 * n << endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
