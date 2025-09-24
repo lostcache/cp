4918b6fb85f8cf9533db3cd9112898d41bcdfc9a
Harsh Devisha <harshdevisha9111@gmail.com>
2025-09-24 16:00:01 -0700

    adds medium long readable template

---
+++ codeforces/medium_long_template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define FOR(i, a, b) for (int i = a; i <= b; ++i)
+#define FORD(i, a, b) for (int i = a; i >= b; --i)
+#define print(a) cout << (a) << ' ';
+#define println(a) cout << (a) << '\n';
+#define vec vector
+typedef long long ll;
+
+using namespace std;
+
+const int inf = 1e9 + 7;
+const ll ll_inf = 1e19 + 7;
+
+void solve() {}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
