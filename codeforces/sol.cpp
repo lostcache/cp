7d67a608b2595baf53b2a1e13470291d2fccec0a
Harsh Devisha <harshdevisha9111@gmail.com>
2025-10-17 17:45:42 -0700

    cf

---
+++ codeforces/sol.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int int64_t
+
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define FORD(i, a, b) for (int i = (a); i >= (b); --i)
+
+
+#define pi pair<int, int>
+#define vi vector<int>
+#define mi map<int, int>
+#define si set<int>
+#define vvi vector<vi>
+#define f first
+#define s second
+
+#define vpi vector<pi>
+
+#define pb push_back
+#define mp make_pair
+
+const int inf = 1e15+7;
+const int MOD = 1e9+7;
+
+void solve() {
+}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
